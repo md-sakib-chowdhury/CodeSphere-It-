7f618a716f8ae5ea1815758148d8f22b7190d0bb
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-06-22 08:11:18 +0600

    suthRoutes

--- codesphere-backend/routes/authRoutes.js
+++ codesphere-backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { login, register, getMe } = require('../controllers/authController');
+const { login, register, getMe, checkSetup } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
+router.get('/check-setup', checkSetup);
 router.post('/login', login);
 router.post('/register', register);
 router.get('/me', protect, getMe);
