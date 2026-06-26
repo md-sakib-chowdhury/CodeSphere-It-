a9b91dd5a93859f89a703872c7b1c4a46d6df59c
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-06-26 08:53:45 +0600

    home jsx new

--- codesphere-frontend/src/pages/Home.jsx
+++ codesphere-frontend/src/pages/Home.jsx
@@ -1,10 +1,36 @@
+// import Navbar from '../components/Navbar/Navbar'
+// import Hero from '../components/Hero/Hero'
+// import Services from '../components/Services/Services'
+// import Portfolio from '../components/Portfolio/Portfolio'
+// import Team from '../components/Team/Team'
+// import Testimonials from '../components/Testimonials/Testimonials'
+// import Contact from '../components/Contact/Contact'
+// import Footer from '../components/Footer/Footer'
+
+// export default function Home() {
+//     return (
+//         <>
+//             <Navbar />
+//             <Hero />
+//             <Services />
+//             <Portfolio />
+//             <Team />
+//             <Testimonials />
+//             <Contact />
+//             <Footer />
+//         </>
+//     )
+// }
 import Navbar from '../components/Navbar/Navbar'
 import Hero from '../components/Hero/Hero'
 import Services from '../components/Services/Services'
+import WhyChooseUs from '../components/WhyChooseUs/WhyChooseUs'
 import Portfolio from '../components/Portfolio/Portfolio'
+import KeyCompetency from '../components/KeyCompetency/KeyCompetency'
+import StatsCards from '../components/StatsCards/StatsCards'
 import Team from '../components/Team/Team'
 import Testimonials from '../components/Testimonials/Testimonials'
-import Contact from '../components/Contact/Contact'
+import LatestActivities from '../components/LatestActivities/LatestActivities'
 import Footer from '../components/Footer/Footer'
 
 export default function Home() {
@@ -13,10 +39,13 @@ export default function Home() {
             <Navbar />
             <Hero />
             <Services />
+            <WhyChooseUs />
             <Portfolio />
+            <KeyCompetency />
+            <StatsCards />
             <Team />
             <Testimonials />
-            <Contact />
+            <LatestActivities />
             <Footer />
         </>
     )
