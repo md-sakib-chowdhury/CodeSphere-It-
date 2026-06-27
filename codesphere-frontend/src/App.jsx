c6e0f290463dc4e0782edd30ead65a7e88155a7b
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-06-27 08:33:17 +0600

    app jsx new

--- codesphere-frontend/src/App.jsx
+++ codesphere-frontend/src/App.jsx
@@ -1,8 +1,27 @@
+// import { Routes, Route } from 'react-router-dom'
+// import Home from './pages/Home'
+// import AdminLogin from './pages/AdminLogin'
+// import ExploreUs from './pages/ExploreUs/ExploreUs'
+// import LatestArticles from './pages/LatestArticles/LatestArticles'
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/admin" element={<AdminLogin />} />
+//       <Route path="/explore-us" element={<ExploreUs />} />
+//       <Route path="/latest-articles" element={<LatestArticles />} />
+//     </Routes>
+//   )
+// }
+
+// export default App
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import AdminLogin from './pages/AdminLogin'
 import ExploreUs from './pages/ExploreUs/ExploreUs'
 import LatestArticles from './pages/LatestArticles/LatestArticles'
+import ServicesPage from './pages/ServicesPage/ServicesPage'
 
 function App() {
   return (
@@ -11,6 +30,7 @@ function App() {
       <Route path="/admin" element={<AdminLogin />} />
       <Route path="/explore-us" element={<ExploreUs />} />
       <Route path="/latest-articles" element={<LatestArticles />} />
+      <Route path="/services" element={<ServicesPage />} />
     </Routes>
   )
 }
