0ab134971b366b0a3fc70eb9aa2be3cd8ed1a29f
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-07-01 08:58:44 +0600

    new app jsx

--- codesphere-frontend/src/App.jsx
+++ codesphere-frontend/src/App.jsx
@@ -4,10 +4,7 @@ import AdminLogin from './pages/AdminLogin'
 import ExploreUs from './pages/ExploreUs/ExploreUs'
 import LatestArticles from './pages/LatestArticles/LatestArticles'
 import ServicesPage from './pages/ServicesPage/ServicesPage'
-
-// 🎯 Career এবং Contact এর কম্পোনেন্ট বা পেইজ দুটি এখানে ইম্পোর্ট করা হলো
-// (আপনার প্রজেক্টের ফোল্ডার স্ট্রাকচার অনুযায়ী পাথ ঠিক রাখা হয়েছে)
-
+import ServiceDetails from './pages/ServiceDetails/ServiceDetails'
 import Contact from './components/Contact/Contact'
 
 function App() {
@@ -18,7 +15,7 @@ function App() {
       <Route path="/explore-us" element={<ExploreUs />} />
       <Route path="/latest-articles" element={<LatestArticles />} />
       <Route path="/services" element={<ServicesPage />} />
-    
+      <Route path="/services/:slug" element={<ServiceDetails />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
   )
