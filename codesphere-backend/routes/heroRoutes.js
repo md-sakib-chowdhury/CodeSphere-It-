c524a7c2610ea8017fa1dc7e7cd4457fcf363f1b
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-07-08 10:01:47 +0600

    ero routes

--- codesphere-backend/routes/heroRoutes.js
+++ codesphere-backend/routes/heroRoutes.js
@@ -1,6 +1,27 @@
+// const router = require('express').Router();
+// const Hero = require('../models/Hero');
+// const { protect } = require('../middleware/authMiddleware');
+// router.get('/', async (req, res) => {
+//     try {
+//         let h = await Hero.findOne();
+//         if (!h) h = await Hero.create({ typedWords: ['Web Applications', 'E-commerce Stores', 'SaaS Platforms', 'Custom Software'] });
+//         res.json(h);
+//     } catch (err) { res.status(500).json({ message: err.message }); }
+// });
+// router.put('/', protect, async (req, res) => {
+//     try {
+//         let h = await Hero.findOne();
+//         if (!h) h = new Hero();
+//         Object.assign(h, req.body);
+//         await h.save();
+//         res.json(h);
+//     } catch (err) { res.status(500).json({ message: err.message }); }
+// });
+// module.exports = router;
 const router = require('express').Router();
 const Hero = require('../models/Hero');
-const { protect } = require('../middleware/authMiddleware');
+const { protect, checkPermission } = require('../middleware/authMiddleware');
+
 router.get('/', async (req, res) => {
     try {
         let h = await Hero.findOne();
@@ -8,7 +29,8 @@ router.get('/', async (req, res) => {
         res.json(h);
     } catch (err) { res.status(500).json({ message: err.message }); }
 });
-router.put('/', protect, async (req, res) => {
+
+router.put('/', protect, checkPermission('manageHero'), async (req, res) => {
     try {
         let h = await Hero.findOne();
         if (!h) h = new Hero();
@@ -17,4 +39,5 @@ router.put('/', protect, async (req, res) => {
         res.json(h);
     } catch (err) { res.status(500).json({ message: err.message }); }
 });
+
 module.exports = router;
