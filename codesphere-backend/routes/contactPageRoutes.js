849309c6ddbecf3a843d8d6d47cfd3b6b375848c
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-07-19 08:10:36 +0600

    contract page route

--- codesphere-backend/routes/contactPageRoutes.js
+++ codesphere-backend/routes/contactPageRoutes.js
@@ -1,6 +1,31 @@
+// const router = require('express').Router();
+// const ContactPage = require('../models/ContactPage');
+// const { protect, checkPermission } = require('../middleware/authMiddleware');
+
+// router.get('/', async (req, res) => {
+//     try {
+//         let p = await ContactPage.findOne();
+//         if (!p) p = await ContactPage.create({});
+//         res.json(p);
+//     } catch (err) { res.status(500).json({ message: err.message }); }
+// });
+
+// router.put('/', protect, checkPermission('manageContactPage'), async (req, res) => {
+//     try {
+//         let p = await ContactPage.findOne();
+//         if (!p) p = new ContactPage();
+//         Object.assign(p, req.body);
+//         await p.save();
+//         res.json(p);
+//     } catch (err) { res.status(500).json({ message: err.message }); }
+// });
+
+// module.exports = router;
 const router = require('express').Router();
 const ContactPage = require('../models/ContactPage');
 const { protect, checkPermission } = require('../middleware/authMiddleware');
+const { uploadImage } = require('../controllers/crudFactory');
+const cloudinary = require('cloudinary').v2;
 
 router.get('/', async (req, res) => {
     try {
@@ -14,7 +39,18 @@ router.put('/', protect, checkPermission('manageContactPage'), async (req, res) 
     try {
         let p = await ContactPage.findOne();
         if (!p) p = new ContactPage();
-        Object.assign(p, req.body);
+        const data = { ...req.body };
+
+        if (data.bannerImage?.startsWith('data:')) {
+            if (p.bannerImagePublicId) {
+                await cloudinary.uploader.destroy(p.bannerImagePublicId).catch(() => { });
+            }
+            const { url, publicId } = await uploadImage(data.bannerImage);
+            data.bannerImage = url;
+            data.bannerImagePublicId = publicId;
+        }
+
+        Object.assign(p, data);
         await p.save();
         res.json(p);
     } catch (err) { res.status(500).json({ message: err.message }); }
